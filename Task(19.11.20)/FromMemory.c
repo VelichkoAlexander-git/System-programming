ae84a222910feda7463dd737ec82f7ef4462387b
Alexander Velichko <Alexander-Velichko@bk.ru>
2020-12-01 09:36:52 +0200

    fix

---
+++ Task(19.11.20)/FromMemory.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+
+int main()
+{
+	key_t key = ftok("shmfile", 65);
+	int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+	char *str = (char*) shmat(shmid, (void*)0,0);	
+	printf("Data %s\n", str);
+	shmdt(str);
+	shmctl(shmid, IPC_RMID, NULL);
+
+	return 0;
+}
